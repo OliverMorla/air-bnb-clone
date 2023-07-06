4f0321b42d1fec3d26c78c9f93d8ab431b3aa570
Oliver Morla <olivermiguel1129@gmail.com>
2023-07-06 15:21:48 -0400

    adding categories

---
+++ client/src/pages/explore/categories/index.tsx
@@ -0,0 +1,9 @@
+const Categories = () => {
+    return (
+        <>
+            <img src="./assets/categories/House.svg" alt="" />
+        </>
+    );
+}
+ 
+export default Categories;
--- client/src/pages/explore/index.tsx
+++ client/src/pages/explore/index.tsx
@@ -1,11 +1,14 @@
 import { useLoaderData } from "react-router-dom";
+import Categories from "./categories";
 import "./style.scss";
 
 const Explore = () => {
   const listings = useLoaderData()
-  
   return (
     <main className="explore-wrapper">
+      <section>
+        <Categories />
+      </section>
       <h1> Explore page </h1>
     </main>
   );
