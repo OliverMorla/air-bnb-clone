050014b8b5afe341ce35ac58cc3571b7957c15ad
Oliver Morla <olivermiguel1129@gmail.com>
2023-07-07 13:25:33 -0400

    fixing interfaces

--- client/src/pages/explore/categories/index.tsx
+++ client/src/pages/explore/categories/index.tsx
@@ -1,33 +1,36 @@
 const Categories = () => {
-    return (
-        <>
-            <img src="./assets/categories/House.svg" alt="" />
-            <img src="./assets/categories/Farm.svg" alt="" />
-            <img src="./assets/categories/Cabin.svg" alt="" />
-            <img src="./assets/categories/Apartment.svg" alt="" />
-            <img src="./assets/categories/Cave.svg" alt="" />
-            <img src="./assets/categories/Bed.svg" alt="" />
-            <img src="./assets/categories/Castle.svg" alt="" />
-            <img src="./assets/categories/Container.svg" alt="" />
-            <img src="./assets/categories/Guest.svg" alt="" />
-            <img src="./assets/categories/Hotel.svg" alt="" />
-            <img src="./assets/categories/Barn.svg" alt="" />
-            <img src="./assets/categories/Windmill.svg" alt="" />
-            <img src="./assets/categories/Tent.svg" alt="" />
-            <img src="./assets/categories/Trullo.svg" alt="" />
-            <img src="./assets/categories/Raid.svg" alt="" />
-            <img src="./assets/categories/Houseboat.svg" alt="" />
-            <img src="./assets/categories/Dome.svg" alt="" />
-            <img src="./assets/categories/Yurt.svg" alt="" />
-            <img src="./assets/categories/TreeHouse.svg" alt="" />
-            <img src="./assets/categories/TinyHome.svg" alt="" />
-            <img src="./assets/categories/Ryokan.svg" alt="" />
-            <img src="./assets/categories/Dammuso.svg" alt="" />
-            <img src="./assets/categories/CycladicHome.svg" alt="" />
-            <img src="./assets/categories/CasaParticular.svg" alt="" />
-            <img src="./assets/categories/Campervan.svg" alt="" />
-        </>
-    );
-}
- 
+  return (
+    <>
+      <img src="./assets/categories/House.svg" alt="" className="" />
+      <img src="./assets/categories/Farm.svg" alt="" className="" />
+      <img src="./assets/categories/Cabin.svg" alt="" className="" />
+      <img src="./assets/categories/Apartment.svg" alt="" className="" />
+      <img src="./assets/categories/Cave.svg" alt="" className="" />
+      <img src="./assets/categories/Bed.svg" alt="" className="" />
+      <img src="./assets/categories/Castle.svg" alt="" className="" />
+      <img src="./assets/categories/Container.svg" alt="" className="" />
+      <img src="./assets/categories/Guest.svg" alt="" className="" />
+      <img src="./assets/categories/Hotel.svg" alt="" className="" />
+      <img src="./assets/categories/Barn.svg" alt="" className="" />
+      <img src="./assets/categories/Windmill.svg" alt="" className="" />
+      <img src="./assets/categories/Tent.svg" alt="" className="" />
+      <img src="./assets/categories/Trullo.svg" alt="" className="" />
+      <img src="./assets/categories/Raid.svg" alt="" className="" />
+      <img src="./assets/categories/Houseboat.svg" alt="" className="" />
+      <img src="./assets/categories/Dome.svg" alt="" className="" />
+      <img src="./assets/categories/Yurt.svg" alt="" className="" />
+      <img src="./assets/categories/TreeHouse.svg" alt="" className="" />
+      <img src="./assets/categories/TinyHome.svg" alt="" className="" />
+      <img src="./assets/categories/Ryokan.svg" alt="" className="" />
+      <img src="./assets/categories/Dammuso.svg" alt="" className="" />
+      <img src="./assets/categories/CycladicHome.svg" alt="" className="" />
+      <img src="./assets/categories/CasaParticular.svg" alt="" className="" />
+      <img src="./assets/categories/Piano.svg" alt="" className="" />
+      <img src="./assets/categories/Pool.svg" alt="" className="" />
+      <img src="./assets/categories/Patio.svg" alt="" className="" />
+      <img src="./assets/categories/Fireplace.svg" alt="" className="" />
+    </>
+  );
+};
+
 export default Categories;
--- client/src/pages/explore/index.tsx
+++ client/src/pages/explore/index.tsx
@@ -1,13 +1,43 @@
 import Categories from "./categories";
+import { useLoaderData } from "react-router-dom";
 import "./style.scss";
 
+interface RecordsProps {
+  recordid: string;
+  price: number;
+  name: string;
+  host_neighbourhood: string,
+  city: string,
+  xl_picture_url: string,
+}
+// const getRecords = (records: any) => {
+//   return records
+// }
+
 const Explore = () => {
+  // const records = getRecords(useLoaderData())
+  const records = useLoaderData() as RecordsProps[];
+  records.map((records) => (console.log(records.recordid)))
   return (
     <main className="explore-wrapper">
       <section className="categories-wrapper">
-        <Categories />
+        <div className="categories-scroll">
+          <Categories />
+        </div>
       </section>
-      <div className="listing-wrapper"></div>
+      <div className="listing-wrapper">
+        {records.map((record) => (
+          <div className="card" key={record.recordid}>
+            <img src={record.xl_picture_url} alt="" />
+            <div className="card-content">
+              <p className="location">{`${record.host_neighbourhood},${record.city}`}</p>
+              <p className="nights">{}</p>
+              <p className="price">{record.price}</p>
+            </div>
+            
+          </div>
+        ))}
+      </div>
     </main>
   );
 };
